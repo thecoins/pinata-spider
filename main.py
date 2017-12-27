83306fc89b2b5a9e1578665dec5acd4c93aac4d1
Alberto <xchao@Albertos-MacBook-Pro.local>
2017-12-27 11:01:49 +0800

    add timestamp and rank

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import bs4
-
+import datetime
+import time
 from peewee import *
 
 #db = SqliteDatabase('./exchange.db')
@@ -8,16 +9,19 @@ from peewee import *
 db = MySQLDatabase(host = '127.0.0.1', user = 'root', passwd = '123456', database = 'exchange')
 
 class Exchange(Model):
+    rank = IntegerField()
     name = CharField()
     volume = CharField()      
     url = CharField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
 
     class Meta:
         database = db
 
 db.connect()
 
-#db.drop_tables([Exchange])
+db.drop_tables([Exchange])
 
 db.create_tables([Exchange])
 
@@ -29,9 +33,10 @@ soup = bs4.BeautifulSoup(response.text,"html.parser")
 
 html = soup.select('.volume-header a')
 
-for item in html:
+for index,item in enumerate(html):
 
     link = "https://coinmarketcap.com" + item.attrs['href']
+    time.sleep(3)
     subpage = requests.get(link)
 
     subsoup = bs4.BeautifulSoup(subpage.text,"html.parser")
@@ -40,12 +45,13 @@ for item in html:
     volume = subsoup.select('.text-large2')
     url = subsoup.select('.list-unstyled a')
 
+    print index + 1
     print name[0].text
     print volume[0].text
     print url[0].text
 
 
-    exchange = Exchange(name=name[0].text,volume=volume[0].text,url=url[0].text)
+    exchange = Exchange(rank=index+1,name=name[0].text,volume=volume[0].text,url=url[0].text)
     exchange.save()
 
 db.close()
--- readme.md
+++ readme.md
@@ -6,4 +6,5 @@ requirement
 sudo pip install requests
 sudo pip install beautifulsoup4
 sudo pip install peewee
+sudo pip install pymysql
 ```
