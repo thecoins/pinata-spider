884e7b48ef6271fc4101605616af39f989aa1561
xchao <xchao0213@gmail.com>
2018-06-16 10:29:45 +0800

    exchangeVolume

--- exchange.info.py
+++ exchange.info.py
@@ -9,6 +9,7 @@ from peewee import *
 
 db = MySQLDatabase(host = '127.0.0.1', user = 'root', passwd = '123456', database = 'coinmarketcap')
 
+# Exchange basic info
 class ExchangeInfo(Model):
     rank = IntegerField()
     name = CharField()
@@ -24,11 +25,36 @@ class ExchangeInfo(Model):
     class Meta:
         database = db
 
+# Exchange 24h volume every 5 minute
+class Exchange(Model):
+    rank = IntegerField()
+    name = CharField()
+    volume = CharField()  
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+# Exchange 12h volume and 24h volume
+class ExchangeVolume(Model):
+    name = CharField()
+    volume12 = TextField()  
+    volume24 = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db          
+
 db.connect()
 
 if not ExchangeInfo.table_exists():
   db.create_tables([ExchangeInfo])
+if not Exchange.table_exists():
+  db.create_tables([Exchange])  
+if not ExchangeVolume.table_exists():
+  db.create_tables([ExchangeVolume])    
 
+# index page
 response = requests.get('https://coinmarketcap.com/exchanges/volume/24-hour/all/')
 
 soup = bs4.BeautifulSoup(response.text,"html.parser")
@@ -43,7 +69,7 @@ for index,item in enumerate(tr):
         nick = item.attrs['id']
         print nick
         rank = rank + 1
-
+        # exchange page
         link = "https://coinmarketcap.com/exchanges/" + nick
         time.sleep(3)
         subpage = requests.get(link)
@@ -55,7 +81,7 @@ for index,item in enumerate(tr):
         print name              
 
         urlobj = subsoup.select('.col-xs-12 .list-unstyled a')
-
+        # Exchange property
         fees = ''
         chat = ''
         blog = ''
@@ -78,17 +104,37 @@ for index,item in enumerate(tr):
                 twitter = href   
 
         updatedAt = datetime.datetime.now()
-
-        query = (ExchangeInfo
+        
+        # Query volume from Exchange to ExchangeVolume
+        volume12 = []
+        query12 = Exchange.select(Exchange.volume).where(Exchange.name == nick).order_by(Exchange.timestamp.desc()).limit(144).dicts()
+        for item12 in query12:
+          volume12.append(float(str(item12['volume'])))
+        volume24 = []
+        query24 = Exchange.select(Exchange.volume).where(Exchange.name == nick).order_by(Exchange.timestamp.desc()).limit(288).dicts()
+        for item24 in query24:
+          volume24.append(float(str(item24['volume'])))
+        
+        queryVolume = (ExchangeVolume
+                .update(volume12=volume12,volume24=volume24,timestamp=updatedAt)
+                .where(ExchangeVolume.name == nick))
+        volumerows =  queryVolume.execute()    
+        if volumerows == 0:
+            print 'new ExchangeVolume: ' + name     
+            exchangevolume = ExchangeVolume(name=nick,volume12=volume12,volume24=volume24)
+            exchangevolume.save()  
+        
+        # Update Exchange Rank
+        queryExchange = (ExchangeInfo
                 .update(rank=rank,timestamp=updatedAt,alive=True)
                 .where(ExchangeInfo.name == name))
-        rows =  query.execute()    
+        rows =  queryExchange.execute()    
         if rows == 0:
             print 'new Exchange: ' + name     
             exchangeinfo = ExchangeInfo(rank=rank,name=name,nick=nick,fees=fees,chat=chat,blog=blog,url=url,twitter=twitter)
             exchangeinfo.save()   
 
-
+# Set Exchange unalive if No more updates
 thistime = datetime.datetime.now()
 yesterday = thistime - datetime.timedelta(days=1)
 yesterdayLast = yesterday.replace(hour=0,minute=0,second=0,microsecond=0)
@@ -98,8 +144,6 @@ query = (ExchangeInfo
             .where(ExchangeInfo.timestamp < yesterdayLast))
 rows =  query.execute()    
 print rows        
-    
-    
-
+        
 db.close()
 
